ae5a4a79d70af56b70a934a8751a633c560c0890
Sai Krishna <saikrishna106@yahoo.com>
2018-02-04 10:47:32 +0530

    arthimetic

--- operators/Arthimetic/main.c
+++ operators/Arthimetic/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
  int main(){
-     int a,b,sum,sub,multi,div;
+     int a,b,sum,sub,multi,div,rem;
      printf("Enter the values of a and b\n");/*Prints the values of a and b*/
      scanf("%d%d",&a,&b);/*Reads the value of a and b*/
      sum=a+b;
@@ -16,6 +16,8 @@
      div=a/b;
      /*Division of two numbers*/
      printf("\n div of %d and %d is:%d",a,b,div);
+     rem=a%b;
+     printf("\n Reminder when %d divied by %d is:%d",a,b,rem);/*Modulo division*/
      /*exit the program with zero status*/
      return 0;
  }
