317f23eed96dab7c86bb062879e362179133fc75
Sai Krishna <saikrishna106@yahoo.com>
2018-02-04 14:37:37 +0530

    assignment

---
+++ operators/assignment/main.c
@@ -0,0 +1,21 @@
+//ASSIGNMENT OPERATORS (=,+=,-=,*=,/=,%=)
+
+#include<stdio.h>
+
+
+int main(){
+    int a=5,b;
+    b = a;//Here "a" value is assign to "b" value which makes b=a if you take a=b you will meet garbage value:-)
+    printf(" The value of b is:%d\n",b);
+    b+=a;//b=b+a
+    printf("\nThe value of b is:%d",b);
+    b-=a;
+    printf("\nThe value of b is:%d",b);//b=b-a
+    b*=a;
+    printf("\n The value of b is:%d",b);
+    b/=a;//b=b*a
+    printf("\n The value of b is:%d",b);
+    b%=a;//b=b%a(The remainder of b/a)
+    printf("\n The value of b is:%d",b);
+    return 0;
+}
