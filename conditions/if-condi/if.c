d1c7f15e4863f8fb05c745ef5ac6e602ed9519d6
Sai Krishna <saikrishna106@yahoo.com>
2018-02-06 21:42:14 +0530

    if

---
+++ conditions/if-condi/if.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter the values of a and b");
+    scanf("%d %d",&a,&b);
+    if(a>b){
+        printf("\nHere the value of a is greater than b");
+    }
+    printf("\nEND OF PROGRAM");
+}
