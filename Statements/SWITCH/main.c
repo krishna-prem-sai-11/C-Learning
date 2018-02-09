73bef4365773f239291794fe66c7ce9d29e0665f
Sai Krishna <saikrishna106@yahoo.com>
2018-02-09 13:55:06 +0530

    statements

---
+++ Statements/SWITCH/main.c
@@ -0,0 +1,24 @@
+//used to excute the code for multiple  conditions
+
+
+#include<stdio.h>
+
+int main(){
+    int ch;
+    printf("Enter your choice should be b/w 1 and 5");
+    scanf("%d",&ch);
+    switch(ch){
+        case 1:printf("\nyour choice is %d",ch);
+        break;
+        case 2:printf("\nnyour choice is %d",ch);
+        break;
+        case 3:printf("\nyour choice is %d",ch);
+        break;
+        case 4:printf("\nyour choice is %d",ch);
+        break;
+        case 5:printf("\nyour choice is %d",ch);
+        break;
+        default:printf("\nYour choice is wrong choice");
+    }
+    return 0;
+}
