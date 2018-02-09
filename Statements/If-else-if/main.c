7c373dd06b924513f42224564f2835cb4322972d
Sai Krishna <saikrishna106@yahoo.com>
2018-02-09 10:54:29 +0530

    statements

---
+++ Statements/If-else-if/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter the value of a, b, and c:");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b){
+        printf("\nThe value of a>b ");//Here the first condition is printed first because it is excuted first.
+    }
+    else
+    if(a>c){
+        printf("\nThe value  of a>c");//if above condition is false then this condition is excuted
+    }
+    else
+    if(b>c){
+        printf("\nThe value of b>c");//if both above conditions is false then this condition is excuted
+    }
+    else{
+        printf("\nThe value of a<b<c");//if all the above conditions is false then this condition is excuted
+    }
+    return 0;
+}
