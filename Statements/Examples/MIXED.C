8f195977147014bdf3e4e8c783cf78d46e5b561a
Sai Krishna <saikrishna106@yahoo.com>
2018-02-09 12:42:47 +0530

    statements

---
+++ Statements/Examples/MIXED.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter the value of a, b and c");
+    scanf("%d %d %d",&a,&b,&c);
+    if((a>b)&&(a>c)){
+        if(b>c){
+            printf("\nb is greater than c");
+        }
+        else{
+            printf("\na>c>b");
+        }
+    }
+    else
+    if((c>b)&&(c>a)){
+        if(b>a){
+            printf("\nThe value of b > a");
+        }
+        else{
+            printf("\nc>a>b");
+        }
+    }
+    else{
+        printf("\nThe value of b is greater");
+    }
+}
