531dd9ecb41afbcf683d54c9f30b5b11eb99721c
Sai Krishna <saikrishna106@yahoo.com>
2018-02-10 10:22:16 +0530

    loops

---
+++ Loops/while/main.c
@@ -0,0 +1,14 @@
+//While by different methods
+
+#include<stdio.h>
+
+int main(){
+    int i;
+    printf("Enter the value of i\n");
+    scanf("%d",&i);
+    while(i<=10){
+        printf("\n%d\n",i);
+        i++;
+    }
+    return 0;
+}
