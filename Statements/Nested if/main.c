4404aff8eb2d3c7e4a3eb147d432a1f3c41e16b4
Sai Krishna <saikrishna106@yahoo.com>
2018-02-17 11:04:14 +0530

    nested

--- Statements/Nested if/main.c
+++ Statements/Nested if/main.c
@@ -4,17 +4,19 @@
 
 int main(){
     int a,b,c;
-    printf("Enter the value of a and b:");
+    printf("Enter the value of a, b and c:");
     scanf("%d %d %d",&a,&b,&c);
     if(a>b){
+        printf("\nThe value of a is greater then b");
         if(a>c){
             printf("\nThe value of a > c");
         }
         else{
-            printf("\nThe value of a > b only");
+            printf("\nThe value of c > a");
         }
     }
     else{
         printf("\nThe Value of a !> b");
     }
+    return 0;
 }
